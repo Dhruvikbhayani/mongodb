676c40295a2f42b97bc751f7693e9b51eb38e435
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-02-23 16:32:58 +0530

    23-02

--- saleman.js
+++ saleman.js
@@ -1,1 +1,7 @@
-db.saleman.insertMany([{SalemanNo:"S0001",Name:"Aman"}])
+// db.saleman.insertMany([{SalemanNo:"S0001",Name:"Aman",address1:"A/14",address2:"Worli",City:"Mumbai",PinCode:"400002",State:"Maharashtra"},
+// {SalemanNo:"S0002",Name:"Omkar",address1:"654",address2:"Mariman",City:"Mumbai",PinCode:"400001",State:"Maharashtra"},
+// {SalemanNo:"S0003",Name:"Raj",address1:"P-7",address2:"Bandra",City:"Mumbai",PinCode:"400002",State:"Maharashtra"},
+// {SalemanNo:"S0004",Name:"Ashish",address1:"A/5",address2:"Juhu",City:"Mumbai",PinCode:"400004",State:"Maharashtra"}
+// ])
+
+// load("saleman.js")
