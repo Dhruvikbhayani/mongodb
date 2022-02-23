6eadd69b97ffd0410e7fae327da8465fb7e55919
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-02-23 16:13:24 +0530

    23-02

---
+++ product.js
@@ -0,0 +1,10 @@
+// db.productmaster.insert([{productno:"P00001",Description:"T-shirts",profitPercent:5,UnitMeasure:"Piece",QtyonHand:200,Reorderlvi:50,sellprice:350,costprice:250},
+// {productno:"P00001",Description:"Shirts",profitPercent:6,UnitMeasure:"Piece",QtyonHand:150,Reorderlvi:50,sellprice:500,costprice:350},
+// {productno:"P00002",Description:"Cotton jeans",profitPercent:5,UnitMeasure:"Piece",QtyonHand:100,Reorderlvi:20,sellprice:600,costprice:450},
+// {productno:"P00003",Description:"jeans",profitPercent:5,UnitMeasure:"Piece",QtyonHand:100,Reorderlvi:20,sellprice:750,costprice:500},
+// {productno:"P00004",Description:"Trousers",profitPercent:2,UnitMeasure:"Piece",QtyonHand:150,Reorderlvi:50,sellprice:850,costprice:550},
+// {productno:"P00005",Description:"Pull Overs",profitPercent:2.5,UnitMeasure:"Piece",QtyonHand:80,Reorderlvi:30,sellprice:700,costprice:450},
+// {productno:"P00006",Description:"Denim Shirts",profitPercent:4,UnitMeasure:"Piece",QtyonHand:100,Reorderlvi:40,sellprice:350,costprice:250},
+// {productno:"P00007",Description:"Lycra Tops",profitPercent:5,UnitMeasure:"Piece",QtyonHand:70,Reorderlvi:30,sellprice:300,costprice:175},
+// {productno:"P00008",Description:"Skirts",profitPercent:5,UnitMeasure:"Piece",QtyonHand:75,Reorderlvi:30,sellprice:450,costprice:300}
+// ])
