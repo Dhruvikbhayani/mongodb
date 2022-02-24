06a2c3fee541f4ba54dcfb6f63fde9957a1cb1ea
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-02-24 10:22:32 +0530

    24-02

---
+++ salesman_master.js
@@ -0,0 +1,5 @@
+db.salesman_master.insert([{salesmanno:"S00001",salesmanname:"Aman",address1:"A/14",address2:"Worli",city:"Mumbai",pincode:"400002",state:"Maharashtra",salamt:3000,tgttoget:100,ytdsale:50,remark:"Good"},
+{salesmanno:"S00002",salesmanname:"Omkar",address1:"65",address2:"Nariman",city:"Mumbai",pincode:"400001",state:"Maharashtra",salamt:3000,tgttoget:200,ytdsale:100,remark:"Good"},
+{salesmanno:"S00003",salesmanname:"Raj",address1:"P-7",address2:"Bandra",city:"Mumbai",pincode:"400002",state:"Maharashtra",salamt:3000,tgttoget:200,ytdsale:100,remark:"Good"},
+{salesmanno:"S00004",salesmanname:"Ashish",address1:"A/5",address2:"Juhu",city:"Mumbai",pincode:"400004",state:"Maharashtra",salamt:3500,tgttoget:200,ytdsale:150,remark:"Good"}
+])
