ceede32d56d69b90c0530b250aa763973a20c203
Dhruvik bhayani <dkbhayai77@gmail.com>
2022-02-24 11:08:27 +0530

    24-02

---
+++ sales_order.js
@@ -0,0 +1,7 @@
+db.sales_order.insert([{orderno:"O19001",orderdate:"12-June-04",delyaddr:"Surat",salesmanno:"S00001",delytype:"F",billyn:"N",delydate:"20-July-02",orderstatus:"In Process"},
+{orderno:"O19006",orderdate:"25-June-04",delyaddr:"Ahemadabad",salesmanno:"S00002",delytype:"P",billyn:"N",delydate:"27-June-02",orderstatus:"Cancelled"},
+{orderno:"O19002",orderdate:"18-Feb-04",delyaddr:"Bharuch",salesmanno:"S00003",delytype:"F",billyn:"Y",delydate:"20-Feb-02",orderstatus:"Fulfilled"},
+{orderno:"O19003",orderdate:"03-Apr-04",delyaddr:"Navsari",salesmanno:"S00001",delytype:"F",billyn:"Y",delydate:"07-Apr-02",orderstatus:"Fulfilled"},
+{orderno:"O19004",orderdate:"20-May-04",delyaddr:"Valsad",salesmanno:"S00002",delytype:"P",billyn:"N",delydate:"22-May-02",orderstatus:"Cancelled"},
+{orderno:"O19005",orderdate:"24-May-04",delyaddr:"Vapi",salesmanno:"S00003",delytype:"F",billyn:"N",delydate:"26-July-02",orderstatus:"In Process"}
+])
